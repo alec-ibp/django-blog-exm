55533a00ad34b6377c1838d6e424431ecb86d1e4
Alejandro <alejo1ibarra@gmail.com>
2022-02-23 21:07:09 -0500

    templates implementation complete

--- django_blog/apps/blog/urls.py
+++ django_blog/apps/blog/urls.py
@@ -1,7 +1,16 @@
 from .views import home
+from .views import general
+from .views import technology
+from .views import tutorials
+from .views import contact
+
 from django.urls import path
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("general/", general, name="general"),
+    path("technology/", technology, name="technology"),
+    path("contact/", contact, name="contact"),
+    path("tutorials/", tutorials, name="tutorials"),
 ]
--- django_blog/apps/blog/views.py
+++ django_blog/apps/blog/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+
+def general(request):
+    return render(request, "general.html")
+
+
+def technology(request):
+    return render(request, "technology.html")
+
+
+def tutorials(request):
+    return render(request, "tutorials.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
