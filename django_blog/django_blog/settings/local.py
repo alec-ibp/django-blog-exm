df400ac5e30529fa2b8b3f87ca229970bae405d5
Alejandro <alejo1ibarra@gmail.com>
2022-02-26 09:32:00 -0500

    configure settings

--- django_blog/django_blog/settings/local.py
+++ django_blog/django_blog/settings/local.py
@@ -1,5 +1,10 @@
 from .base import *
 
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG', default=False)
+
+ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
--- django_blog/django_blog/settings/production.py
+++ django_blog/django_blog/settings/production.py
@@ -1,5 +1,11 @@
 from .base import *
 
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool(True)
+
+ALLOWED_HOSTS = []
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
