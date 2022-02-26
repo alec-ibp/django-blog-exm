df1543f8b850c67bcd2c2ffbf01276275cefcacc
Alejandro <alejo1ibarra@gmail.com>
2022-02-26 10:30:09 -0500

    configure settings

--- django_blog/django_blog/settings/base.py
+++ django_blog/django_blog/settings/base.py
@@ -3,7 +3,7 @@ import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # init environ
 env = environ.Env()
