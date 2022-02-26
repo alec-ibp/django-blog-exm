9feec26160b6cedd2c8590385c85fbb0bdcd5722
Alejandro <alejo1ibarra@gmail.com>
2022-02-26 09:09:44 -0500

    configuration for deploy to heroku

---
+++ django_blog/django_blog/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASS'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
+    }
+}
